ffe1a2900ab8130e2e01ddd4be99dc94485a6b2c
howardreith <howard.reith@gmail.com>
2018-12-04 11:40:44 -0500

    Add plans resource
    -Functionality for a user joining an event via user_events_plans added

---
+++ scripts/usereventsplans/create-plan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "http://localhost:4741/user_events_plans" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "user_events_plan": {
+      "user_id": "'"${USER_ID}"'",
+      "event_id": "'"${EVENT_ID}"'"
+    }
+  }'
+
+echo
---
+++ scripts/usereventsplans/get-plan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "http://localhost:4741/user_events_plans/${ID}" \
+  --include \
+  --request GET \
+
+echo
