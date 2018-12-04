55fe79ee61b7813a27be5e44ccab783045c14064
howardreith <howard.reith@gmail.com>
2018-12-04 15:10:16 -0500

    Remove group_id from group serializer
    -group_id is useless and has been removed from the serializer
    -In the future, remove the group_id column as well

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,5 +1,5 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :group_id, :description, :created_at,
+  attributes :id, :name, :description, :created_at,
   :updated_at, :creator_id
 
 end
