cdb78a11816150c616d37cb11aa254c73ce82c1b
howardreith <howard.reith@gmail.com>
2018-12-06 13:22:51 -0500

    Minor changes to Heroku invitation scripts
    -Minor changes to scripts for event invitations to heroku

--- scripts/heroku/eventinvitations/create-event-invitation.sh
+++ scripts/heroku/eventinvitations/create-event-invitation.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 curl "http://localhost:4741/event_invitations" \
-  --include \
-  --request POST \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}" \
-  --data '{
-    "event_invitation": {
-      "message": "'"${MESSAGE}"'",
-      "group_inviter_id": "'"${GROUP}"'",
-      "invited_id": "'"${INVITED}"'"
-    }
-  }'
+--include \
+--request POST \
+--header "Content-Type: application/json" \
+--header "Authorization: Token token=${TOKEN}" \
+--data '{
+  "event_invitation": {
+    "message": "'"${MESSAGE}"'",
+    "group_inviter_id": "'"${GROUP}"'",
+    "invited_id": "'"${INVITED}"'",
+    "event_id": "'"${EVENT}"'"
+  }
+}'
 
 echo
--- scripts/heroku/eventinvitations/update-event-invitation.sh
+++ scripts/heroku/eventinvitations/update-event-invitation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 curl "https://snoflake-api.herokuapp.com/event_invitations/${ID}" \
-  --include \
-  --request PATCH \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}" \
-  --data '{
-    "group_invitation": {
-      "accepted": "'"${ACCEPTED}"'",
-      "message": "'"${MESSAGE}"'"
-    }
-  }'
+--include \
+--request PATCH \
+--header "Content-Type: application/json" \
+--header "Authorization: Token token=${TOKEN}" \
+--data '{
+  "event_invitation": {
+    "accepted": "'"${ACCEPTED}"'"
+  }
+}'
 echo
